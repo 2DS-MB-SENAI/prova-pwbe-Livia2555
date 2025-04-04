3cc998a4a238f6c5104699aa89644363fabb4208
Livia Costa Melo <46204667866@>
2025-04-04 11:22:45 -0300

    prova

--- clinica/urls.py
+++ clinica/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('medicos/', views.listar_medico, name= 'listarMedico'),
-    path('consultas/nova/', views.criar_consulta, name= 'criarConsulta')
+    path('consultas/nova/', views.criar_consulta, name= 'form_consulta'),
+    path('consultas/<int:id>/', views.criar_consulta, name= 'consulta'),
 ]
--- clinica/views.py
+++ clinica/views.py
@@ -4,20 +4,24 @@ from .forms import MedicoForm , ConsultaForm
 
 def listar_medico (request):
     listar = Medico.objects.all()
-    return render (request, '.templetes/clinica/listar_medico.html', {'listar':listar})
+    return render (request, 'listar_medico.html', {'listar':listar})
 
 def criar_consulta (request):
     if request.method == 'POST':
         form = ConsultaForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect ('base.html')
+            return redirect ('form_consulta')
         
     else:
         form = ConsultaForm
 
     return render (request, 'criarConsulta', {'from':form})
-        
+
+
+def detalhes_consulta(request):
+      detalhes =  get_list_or_404(Consulta, id)    
+      return render (request,'form_consulta', {'detalhes':detalhes}) 
 
 
 
