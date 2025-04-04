6f158417aba487c5827fce50bf392437292f97fe
Livia Costa Melo <46204667866@>
2025-04-04 11:29:54 -0300

    prova

--- clinica/views.py
+++ clinica/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render , redirect , get_list_or_404
 from .models import Medico , Consulta
 from .forms import MedicoForm , ConsultaForm
 
+
+def Base (request):
+    return render (request, 'clinica/base.html')
+
 def listar_medico (request):
     listar = Medico.objects.all()
-    return render (request, 'listar_medico.html', {'listar':listar})
+    return render (request, 'clinica/listar_medico.html', {'listar':listar})
 
 def criar_consulta (request):
     if request.method == 'POST':
@@ -16,12 +20,12 @@ def criar_consulta (request):
     else:
         form = ConsultaForm
 
-    return render (request, 'criarConsulta', {'from':form})
+    return render (request, 'clinica/form_consulta', {'from':form})
 
 
 def detalhes_consulta(request):
       detalhes =  get_list_or_404(Consulta, id)    
-      return render (request,'form_consulta', {'detalhes':detalhes}) 
+      return render (request,'clinica/form_consulta', {'detalhes':detalhes}) 
 
 
 
